5c8fc7cf6b9fb47c3ad5a64419190e9d1fe195f3
Matthew Lamar Gonzalez <43018662+mattieramos@users.noreply.github.com>
2024-09-22 18:03:21 -0500

    Time: 15 ms (76.21%), Space: 11.8 MB (9.29%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,22 +1,50 @@
 class Solution(object):
     def mySqrt(self, x):
+
+        # You can brute force check every number that leads up to x, but that could take forever and since you have to check every number individually, it would be an O(1) time complexity.
+
+        # will use Binary Search to quickly check middle element. 
+
+        # PSEUDO:
+
+        # create start and end pointers
+        # end pointer will have to be x // 2, since x/2 is the highest a square root would be the highest a perfect squareroot would be. 
+
+        # since 0 and 1 don't have square roots, if x is < 2, return x. 
+
+        # BASE CASE:
+        # while start <= end:
+        #     middle
+        #     will also create a middle_sqrd to check values
+
+        # if middle_sqrd < x
+        #     start = middle + 1
+        # elif middle_sqrd > x
+        #     end = middle - 1
+        # else
+        #     return middle
+        # For non-perfect squares, the computed middle value might exceed the actual square root, requiring us to round down. To handle this, we check if middle * middle > x. If this condition is met, we return middle - 1 to ensure we correctly round down to the nearest integer.
+
+
         start = 1
         end = x // 2
 
         if x < 2:
             return x
-        while start <= end:
-            middle = ((start + end) // 2)
-            middle_sqr = middle * middle
 
-            if middle_sqr < x:
-                start = middle + 1
-            elif middle_sqr > x:
+        while start <= end:
+            middle = (start + end) // 2
+            middle_sqrd = middle * middle
+
+            if middle_sqrd > x:
                 end = middle - 1
+            elif middle_sqrd < x:
+                start = middle + 1
             else:
                 return middle
 
-        if middle_sqr > x:
+        if middle_sqrd > x:
             return middle - 1
         else:
             return middle
+            
