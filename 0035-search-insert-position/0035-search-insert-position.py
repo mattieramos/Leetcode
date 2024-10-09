0a0d24c6f31216cdac838ab87a23948eb6d41bda
Matthew Lamar Gonzalez <43018662+mattieramos@users.noreply.github.com>
2024-10-08 21:46:36 -0500

    Time: 49 ms (57.96%), Space: 17.4 MB (14.76%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.py
+++ 0035-search-insert-position/0035-search-insert-position.py
@@ -1,40 +1,10 @@
-class Solution(object):
-    def searchInsert(self, nums, target):
-       #create array of nums
-       #high = list[nums] - 1
-       #low = nums[0]
-       #mid = (high + low) // 2
-
-       #(base case) while low <= hight
-       #if target > mid
-            #low = mid + 1
-        
-        #elif target < mid
-            # high == mid - 1
-        #else
-            #mid = target
-
-        #return mid
-        
-
-        #*** creating variables ****
-        low = 0
-        high = len(nums) - 1
-        
-        #base case
-        while low <= high:
-            middle = (low + high) // 2
-
-            #if guess (middle) is to low, go higher
-            if nums[middle] < target:
-                low = middle + 1
-            
-            #if guess is too high, go lower
-            elif nums[middle] > target:
-                high = middle - 1
-            
-            # if guess if not higher or lower than target, return it
+class Solution:
+    def searchInsert(self, nums: List[int], target: int) -> int:
+        l,r=0,len(nums)-1
+        while l <= r:
+            mid = (l+r) // 2
+            if nums[mid] < target:
+                l = mid + 1
             else:
-                return middle
-
-        return low
+                r = mid - 1
+        return l
