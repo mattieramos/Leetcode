39822ce2dfd3ce6275c674752463e4f4466d136a
Matthew Lamar Gonzalez <43018662+mattieramos@users.noreply.github.com>
2025-01-13 03:48:59 -0600

    Time: 0 ms (100%), Space: 17.8 MB (21.95%) - LeetHub

--- 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
+++ 1146-greatest-common-divisor-of-strings/1146-greatest-common-divisor-of-strings.py
@@ -1,15 +1,27 @@
 class Solution:
     def gcdOfStrings(self, str1: str, str2: str) -> str:
-        len1, len2 = len(str1), len(str2)
+        lengthOfStr1, lengthOfStr2 = len(str1), len(str2)
 
-        def valid(k):
-            if len1 % k or len2 % k:
+        #helper function
+        def valid(potentialSubstring): 
+
+            #if there's a remainder, string is not divisible by substring.
+            if lengthOfStr1 % potentialSubstring or lengthOfStr2 % potentialSubstring:
                 return False
-            n1, n2 = len1 // k, len2 // k
-            base = str1[:k]
+            
+            #check how many times the string is divisble by the potential substring, and store it in a variable. 
+            n1, n2 = lengthOfStr1 // potentialSubstring, lengthOfStr2 // potentialSubstring
+
+            #using string slicing to get the characters up to the number of characters from the potentialSubString variable to store in to base. 
+            base = str1[:potentialSubstring]
+
+            #create new str1 and str2 to check if it's correct. 
             return str1 == n1 * base and str2 == n2 * base
 
-        for i in range(min(len1, len2), 0, -1):
+        #brute force method that loops through each potential substring of the smallest string (str2), removing a char at the end of the string each time creating all potential substrings. 
+        for i in range(min(lengthOfStr1, lengthOfStr2), 0, -1):
+            #if the substring is divisible and the GCDString, return it. 
             if valid(i):
                 return str1[:i]
+        #else, return null
         return ""
